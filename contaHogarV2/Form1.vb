aec294dfa96bccff5b70ef565c9bf086b1b85ce5
Francisco Sole <francisco.sole@impackta.com>
2016-08-16 17:45:58 +0200

    primer commit, necesitamos documentacion, mockups, etc...

---
+++ contaHogarV2/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class contenedorPrincipal
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+
+
+
+    End Sub
+End Class
